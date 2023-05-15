655f10ed82c6e8dd38cd97656ebba3d8ee1f10c8
waybaba <15305665971@163.com>
2023-05-15 08:33:21 +0000

    sweep

--- policies/main.py
+++ policies/main.py
@@ -159,7 +159,7 @@ path_prefix = ""
 # if $AMLT_DIRSYNC_DIR exists, use it as prefix
 if "AMLT_DIRSYNC_DIR" in os.environ:
     path_prefix = os.environ["AMLT_DIRSYNC_DIR"]
-log_folder = os.path.join(exp_id, system.now_str(), pid)
+log_folder = os.path.join(path_prefix,exp_id, system.now_str(), pid)
 print("log_folder: ", log_folder)
 logger_formats = ["stdout", "log", "csv"]
 if v["eval"]["log_tensorboard"]:
